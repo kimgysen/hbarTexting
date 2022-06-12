e95ade8092f335c943b74fdbaefe0f2a0e91d553
Lorand <Lorand@LAPTOP-O9V8QP7F.home>
2022-06-12 14:47:21 +0200

    Browze NFT whales

--- lib/src/main/java/hbarTexting/WhalesWatcher.java
+++ lib/src/main/java/hbarTexting/WhalesWatcher.java
@@ -2,9 +2,11 @@ package hbarTexting;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.hedera.hashgraph.sdk.AccountId;
 import com.hedera.hashgraph.sdk.Hbar;
@@ -16,7 +18,7 @@ public class WhalesWatcher
 	{
 		List<AccountId> ret = new ArrayList<AccountId>();
 		
-		JsonObject json =JsonReader.readJsonFromUrl("https://mainnet-public.mirrornode.hedera.com/api/v1/accounts?account.balance=gt:50000000000000000");
+		JsonObject json =JsonReader.readJsonFromUrl("https://mainnet-public.mirrornode.hedera.com/api/v1/accounts?account.balance=gt:1000000000000000");
 	    JsonArray jarr = json.getAsJsonArray("accounts");
 		
 	    for (int i=0; i<jarr.size(); i++)
@@ -25,6 +27,51 @@ public class WhalesWatcher
 	    	String contractStr = (""+jo.get("account")).replace("\"", "");
 	    	AccountId accountId = AccountId.fromString(contractStr);
 	    	ret.add(accountId);
+	    }
+	    
+		return ret;
+	}
+	
+	public static List<AccountId> browzeWhalesByNfts(String limit) throws IOException
+	{
+		List<AccountId> ret = new ArrayList<AccountId>();
+		
+		JsonObject json =JsonReader.readJsonFromUrl("https://mainnet-public.mirrornode.hedera.com/api/v1/accounts?account.balance=gt:"+limit+"&account.balance=lt:1000000000000000");
+	    JsonArray jarr = json.getAsJsonArray("accounts");
+	    JsonElement links = json.getAsJsonObject("links").get("next");
+		
+	    //System.out.println("null".equals(""+links));
+	   
+	    
+	    for (int i=0; i<jarr.size(); i++)
+	    {
+	    	JsonObject jo = (JsonObject) jarr.get(i);
+	    	String accountStr = (""+jo.get("account")).replace("\"", "");
+	    	AccountId accountId = AccountId.fromString(accountStr);
+	    	String nft = getAccountNfts(accountId);
+	    	if (null!=nft) ret.add(accountId);
+	    }
+	   
+	    
+	    while (!"null".equals(""+links))
+	    {
+	    	String next = "https://mainnet-public.mirrornode.hedera.com"+(""+links).replace("\"", "");
+	    	System.out.println(next);
+			json =JsonReader.readJsonFromUrl(next);
+		    jarr = json.getAsJsonArray("accounts");
+		    links = json.getAsJsonObject("links").get("next");
+		    
+		    for (int i=0; i<jarr.size(); i++)
+		    {
+		    	JsonObject jo = (JsonObject) jarr.get(i);
+		    	String accountStr = (""+jo.get("account")).replace("\"", "");
+		    	AccountId accountId = AccountId.fromString(accountStr);
+		    	
+		    	String nft = getAccountNfts(accountId);
+		    	if (null!=nft) ret.add(accountId);
+		    }
+		    
+		    System.out.println(links);
 	    }
 	    
 		return ret;
@@ -41,16 +88,48 @@ public class WhalesWatcher
 		return balance;
 	}
 	
+	public static String getAccountNfts(AccountId accountId) throws IOException
+	{
+		String metadata = null;
+		
+		Hbar balance = Hbar.from(0);
+		JsonObject json =JsonReader.readJsonFromUrl("https://mainnet-public.mirrornode.hedera.com/api/v1/accounts/"+accountId+"/nfts");
+	    JsonArray jarr = json.getAsJsonArray("nfts");
+	    
+	    
+	    if (jarr.size()>0)
+	    {
+	    	JsonObject jo = (JsonObject) jarr.get(0);
+	
+	    	String encodedString = (""+jo.get("metadata")).replace("\"", "");	    
+	    	byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+	    	
+	    	metadata = new String(decodedBytes);
+	    }
+	    
+		return metadata;
+	}
+	
 	public static void main(String args[]) throws IOException
 	{
-		List<AccountId> whales = getWhalesAccountList();
+		/*
+		String limit = "10000000000000";	
+		List<AccountId> whales = browzeWhalesByNfts(limit);
 		for (int i=0; i<whales.size(); i++)
 		{	
 			AccountId accountId = whales.get(i); 
 			Hbar balance = getAccountBalance(accountId);
+			String nft = getAccountNfts(accountId);
 			
-			System.out.println("The whale:"+accountId+"\t has an account balance of "+balance);
+			System.out.println("The whale:"+accountId+"\t has an account balance of "+balance+" and NFT: "+nft);
 		}
+		*/
+		
+		JsonObject jo = JsonReader.readJsonFromUrl("ipfs://bafyreib2bdydvokah7snghhaighltakmwsjahasfe3rsqufaqa22hv7muq/metadata.json");
+		System.out.println(jo);
+		
+		
+		
 	}
 	
 
